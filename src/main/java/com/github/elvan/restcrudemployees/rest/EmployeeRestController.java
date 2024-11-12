324e5d13b44cad1f1f9d0c9055c46752435548d2
Elvan Hidayat <elvanhd@gmail.com>
2024-11-12 08:13:57 +0700

    Add POST endpoint for creating new employee records
    - Implement addEmployee method in EmployeeRestController
    - Add POST /employees mapping to handle employee creation requests
    - Reset employee ID to 0 in addEmployee to enforce save behavior
    - Return the newly saved employee object after creation

--- src/main/java/com/github/elvan/restcrudemployees/rest/EmployeeRestController.java
+++ src/main/java/com/github/elvan/restcrudemployees/rest/EmployeeRestController.java
@@ -6,6 +6,8 @@ import com.github.elvan.restcrudemployees.entity.Employee;
 import com.github.elvan.restcrudemployees.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +39,21 @@ public class EmployeeRestController {
     }
 
     return theEmployee;
+  }
+
+  // add mapping for POST /employees - add new employee
+
+  @PostMapping("/employees")
+  public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+    // also just in case they pass an id in JSON ... set id to 0
+    // this is to force a save of new item ... instead of update
+
+    theEmployee.setId(0);
+
+    Employee dbEmployee = employeeService.save(theEmployee);
+
+    return dbEmployee;
   }
 
 }
